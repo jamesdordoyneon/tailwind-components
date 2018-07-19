7d45e265e6d7d42ef75ff20e73a3b66a71bbec3b
James Dordoy <james@neondigital.co.uk>
2018-07-19 10:23:48 +0100

    Set up dashboard.

--- resources/assets/js/components/Index.vue
+++ resources/assets/js/components/Index.vue
@@ -1,40 +1,76 @@
 <template>
-<div class="container mx-auto py-10">
-    <div class="my-12 text-white">
+    <div class="container mx-auto py-10">
+        <div class="my-12 mb-4">
             <h1 class="text-5xl font-light mb-4">
-                Dashboard
+                Dashboard <small>V1</small>
             </h1>
-            <h1 class="text-xl font-light">Welcome back, James</h1>
         </div>
         <div class="flex flex-wrap">
-            <div class="w-full lg:w-1/3 mb-4">
-                <div class="card bg-white lg:mr-2">
+            <div class="w-full lg:w-1/4 mb-4">
+                <div class="card bg-white lg:ml-2">
                     <div class="card-body">
-                        <h1 class="text-5xl text-grey-darkest font-light mb-2">
-                             112.758
-                        </h1>
-                        <span class="text-grey-darker uppercase tracking-wide">Page views</span>
+                        <div class="flex flex-wrap">
+                            <div class="lg:w-1/3 bg-blue card-icon py-4 px-4">
+                                <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                            </div>
+                            <div class="lg:w-2/3 py-4 px-4">
+                                <h1 class="text-5xl text-grey-darkest font-light mb-2">
+                                    112,758
+                                </h1>
+                                <span class="text-grey-darker uppercase tracking-wide">Page views</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="w-full lg:w-1/3 mb-4">
-                <div class="card bg-white lg:mx-2">
+           <div class="w-full lg:w-1/4 mb-4">
+                <div class="card bg-white lg:ml-2">
                     <div class="card-body">
-                        <h1 class="text-5xl text-grey-darkest font-light mb-2">4.632</h1>
-                        <span class="text-grey-darker uppercase tracking-wide">Users registered</span>
+                        <div class="flex flex-wrap">
+                            <div class="lg:w-1/3 bg-red card-icon py-4 px-4">
+                                <i class="fa fa-google-plus" aria-hidden="true"></i>
+                            </div>
+                            <div class="lg:w-2/3 py-4 px-4">
+                                <h1 class="text-5xl text-grey-darkest font-light mb-2">
+                                    52,987
+                                </h1>
+                                <span class="text-grey-darker uppercase tracking-wide">Page views</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="w-full lg:w-1/3 mb-4">
-                <div class="card lg:ml-2">
+            <div class="w-full lg:w-1/4 mb-4">
+                <div class="card bg-white lg:ml-2">
                     <div class="card-body">
-                        <daterange-picker
-                            double
-                            start-date="06/10/2017"
-                            end-date="06/10/2018"
-                            start-place-holders="12/01/2017"
-                            end-place-holders="04/01/2018"
-                            @get-dates="getDates"/>
+                        <div class="flex flex-wrap">
+                            <div class="lg:w-1/3 bg-green card-icon py-4 px-4">
+                                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                            </div>
+                            <div class="lg:w-2/3 py-4 px-4">
+                                <h1 class="text-5xl text-grey-darkest font-light mb-2">
+                                    114
+                                </h1>
+                                <span class="text-grey-darker uppercase tracking-wide">Orders today</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full lg:w-1/4 mb-4">
+                <div class="card bg-white lg:ml-2">
+                    <div class="card-body">
+                        <div class="flex flex-wrap">
+                            <div class="lg:w-1/3 bg-orange card-icon py-4 px-4">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                            </div>
+                            <div class="lg:w-2/3 py-4 px-4">
+                                <h1 class="text-5xl text-grey-darkest font-light mb-2">
+                                    1584
+                                </h1>
+                                <span class="text-grey-darker uppercase tracking-wide">Unique Users</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -98,15 +134,18 @@
                             720
                             <span class="text-grey-dark text-2xl">GB</span>
                         </h1>
+                        <daterange-picker
+                            start-date="06/10/2017"
+                            end-date="06/10/2018"
+                            start-place-holders="12/01/2017"
+                            end-place-holders="04/01/2018"
+                            @get-dates="getDates"/>
                         <span class="text-grey-darker uppercase tracking-wide">Bandwidth used</span>
                     </div>
                 </div>
             </div>
         </div>
-
-
-</div>
-
+    </div>
 </template>
 
 <script>
